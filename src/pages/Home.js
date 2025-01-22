9db2124061d30146b810d366386382e7e4b95580
Hacklivly <128738691+hacklivly@users.noreply.github.com>
2025-01-22 11:47:07 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="online-users">
+        <h2>Users Online</h2>
+        {/* Display online users */}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
